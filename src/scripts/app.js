060ad8a7ca29ecd13753c60fd81583fc4c4d0024
Yuriy Demchenko <deuriy@yandex.ru>
2023-08-16 14:22:41 +0300

    Editing client accordion panels

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -47,19 +47,35 @@ $(() => {
 	  loop: true
 	});
 
+	let accordionPanelTimer = null;
+
 	$('.client-accordion-panel__short').click(function(e) {
+		e.preventDefault();
+
+		if (accordionPanelTimer) return;
+
 		let $accordionPanel = $(this).closest('.client-accordion-panel');
 		let $accordionPanelsContainer = $accordionPanel.closest('.client-accordion-panels');
-		let $prevAccordionPanels = $accordionPanel.prevAll(`.client-accordion-panel`);
+		let $activeVisiblePanelItem = $accordionPanelsContainer.find('.client-accordion-panels__item:first-child');
 
-		$prevAccordionPanels.removeClass('client-accordion-panel--opened');
+		let $prevAccordionPanels = $accordionPanel.prevUntil(`.client-accordion-panel--opened`);
+		$prevAccordionPanels = $prevAccordionPanels.add($activeVisiblePanelItem);
+
+		$prevAccordionPanels.removeClass('client-accordion-panel--opened').addClass('client-accordion-panel--closed');
 		$accordionPanel.addClass('client-accordion-panel--opened');
-		$accordionPanelsContainer.append($prevAccordionPanels);
+		$accordionPanelsContainer.find(`.client-accordion-panels__item:nth-child(-n+${$prevAccordionPanels.length + 4})`).removeClass('client-accordion-panel--invisible');
 
-		e.preventDefault();
+		accordionPanelTimer = setTimeout(() => {
+			$accordionPanelsContainer.append($prevAccordionPanels);
+			$prevAccordionPanels.removeClass('client-accordion-panel--closed').addClass('client-accordion-panel--invisible');
+
+			accordionPanelTimer = null;
+		}, 1000);
 	});
 
 	$('.portfolio-slider-section__prev-btn--desktop').click(function(event) {
+		if (accordionPanelTimer) return;
+
 		let $currentPortfolioSection = $(this).closest('.portfolio-slider-section');
 		let $accordionPanelsContainer = $currentPortfolioSection.find('.client-accordion-panels');
 		let $activeVisiblePanelItem = $currentPortfolioSection.find('.client-accordion-panels .client-accordion-panels__item:first-child');
@@ -67,18 +83,37 @@ $(() => {
 
 		$activeVisiblePanelItem.removeClass('client-accordion-panel--opened');
 		$prevPanelItem.addClass('client-accordion-panel--opened');
+		// $prevPanelItem.addClass('client-accordion-panel--opened');
 		$accordionPanelsContainer.prepend($prevPanelItem);
+
+		$accordionPanelsContainer.find('.client-accordion-panels__item:first-child').removeClass('client-accordion-panel--invisible');
+		$accordionPanelsContainer.find('.client-accordion-panels__item:nth-child(5)').addClass('client-accordion-panel--invisible');
 	});
 
 	$('.portfolio-slider-section__next-btn--desktop').click(function(event) {
+		if (accordionPanelTimer) return;
+		
 		let $currentPortfolioSection = $(this).closest('.portfolio-slider-section');
 		let $accordionPanelsContainer = $currentPortfolioSection.find('.client-accordion-panels');
 		let $activeVisiblePanelItem = $currentPortfolioSection.find('.client-accordion-panels .client-accordion-panels__item:first-child');
 		let $nextPanelItem = $activeVisiblePanelItem.next();
 
-		$activeVisiblePanelItem.removeClass('client-accordion-panel--opened');
+		// $accordionPanelsContainer.addClass('client-accordion-panels--loading');
+		$activeVisiblePanelItem.removeClass('client-accordion-panel--opened').addClass('client-accordion-panel--closed');
 		$nextPanelItem.addClass('client-accordion-panel--opened');
-		$accordionPanelsContainer.append($activeVisiblePanelItem);
+		$accordionPanelsContainer.find('.client-accordion-panels__item:nth-child(5)').removeClass('client-accordion-panel--invisible');
+		// $nextPanelItem.next().removeClass('client-accordion-panel--invisible');
+		// $accordionPanelsContainer.append($activeVisiblePanelItem.clone());
+
+		accordionPanelTimer = setTimeout(() => {
+			$activeVisiblePanelItem.addClass('client-accordion-panel--invisible');
+			$accordionPanelsContainer.append($activeVisiblePanelItem);
+			// $accordionPanelsContainer.find('.client-accordion-panels__item:last-child').removeCl;
+			// $accordionPanelsContainer.removeClass('client-accordion-panels--loading');
+			// $accordionPanelsContainer.find(`.client-accordion-panels:nth-child(n+5)`).addClass('hidden');
+			$activeVisiblePanelItem.removeClass('client-accordion-panel--closed');
+			accordionPanelTimer = null;
+		}, 1000);
 	});
 
 	$('.portfolio-slider-section__prev-btn--mobile').click(function(e) {
